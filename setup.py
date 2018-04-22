279d67225c5d80f8d84712301e5f66931045065d
Julio Faerman <jfaerman@gmail.com>
2018-04-22 07:50:32 -0700

    Initial translation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ A part of a TARDIS that allows for the instantaneous translation of most languag
 """
 from setuptools import find_packages, setup
 
-dependencies = ['click', "python-frontmatter", "boto3"]
+dependencies = ['click', "python-frontmatter", "boto3", "markdown"]
 
 setup(
     name='tcirc',
--- tcirc/cli.py
+++ tcirc/cli.py
@@ -4,6 +4,7 @@ import frontmatter
 import boto3
 import io
 import copy
+import hashlib
 
 
 def log(message):
@@ -17,22 +18,58 @@ def isTranslatable(fname):
     return isTranslatable
 
 
+def translate_text(text, src_lang, dst_lang):
+    if (not text):
+        return ""
+    m = hashlib.md5()
+    m.update(text.encode('utf-8'))
+    digest = m.hexdigest()
+    translated_text = ""
+    translate = boto3.client('translate')
+    response = translate.translate_text(
+        Text=text,
+        SourceLanguageCode=src_lang,
+        TargetLanguageCode=dst_lang
+    )
+    translated_text = response['TranslatedText']
+    print("== " + digest + "[MISS] ==")
+    print(text)
+    print("---")
+    print(translated_text)
+    print("====")
+    return translated_text
+
+
 def translate_post(filePath, src_lang, dst_lang):
     root, ext = os.path.splitext(filePath)
     dst_file = root + "." + dst_lang+ext
     if os.path.exists(dst_file):
-        dst_file += ".tcirc"
+        dst_file = root + "." + dst_lang+"_"+ext
 
     dst_path = dst_file
     dst_post = frontmatter.load(filePath)
     dst_post["dst_lang"] = dst_lang
     f = io.open(dst_path, 'wb')
+    content = dst_post.content
+    translation = ""
+    token = ""
+    for char in content:
+        if (char == "." or char == "\n"):
+            translated_text = translate_text(token, src_lang, dst_lang)
+            token = ""
+            translated_text += char
+            translation += translated_text
+        else:
+            token += char
+    if (token):
+        translation += translate_text(token, src_lang, dst_lang)
+    dst_post.content = translation
     frontmatter.dump(dst_post, f)
     f.close()
     print('\t => %s' % dst_path)
 
 
-def translate(dirName, fname, src_lang="en", dst_langs=["en", "es"]):
+def translate(dirName, fname, src_lang="en", dst_langs=["es"]):
     filePath = os.path.join(dirName, fname)
     log('\t%s' % filePath)
     for dst_lang in dst_langs:
